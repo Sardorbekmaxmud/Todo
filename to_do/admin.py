f09186420f44f92be3ea182165fa6788f1d72f52
Sardorbekmaxmud <sardorbekmaxmudov33@gmail.com>
2025-02-15 19:32:57 +0500

    add auto undone todos, each page bg-img and change files path

--- to_do/admin.py
+++ to_do/admin.py
@@ -1,12 +1,12 @@
 from django.contrib import admin
-from .models import ToDo, ToDoRepeat
+from .models import ToDo, ToDoRepeat, ToDoHistory
 
 
 # Register your models here.
 @admin.register(ToDo)
 class ToDoAdmin(admin.ModelAdmin):
-    list_display = ('author', 'body', 'created_at', 'done')
-    list_filter = ('done', 'created_at', 'author')
+    list_display = ('author', 'body', 'done', 'created_at', 'updated_at')
+    list_filter = ('created_at', 'author__username', 'done')
     search_fields = ('body', 'author__username')
     ordering = ('created_at',)
     readonly_fields = ['created_at', 'updated_at']
@@ -17,3 +17,15 @@ class ToDoRepeatAdmin(admin.ModelAdmin):
     list_display = ('todo__body', 'repeat_day')
     list_filter = ('repeat_day',)
     search_fields = ('todo__body',)
+
+
+@admin.register(ToDoHistory)
+class ToDoHistoryAdmin(admin.ModelAdmin):
+    list_display = ('todo__body', 'complete_date', 'updated_at')
+    list_filter = ('complete_date',)
+    ordering = ('complete_date', 'updated_at')
+    readonly_fields = ['complete_date', 'updated_at']
+
+    class Meta:
+        db_name = 'todo_history'
+        verbose_name_plural = 'todo_histories'
