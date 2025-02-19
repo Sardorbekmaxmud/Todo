576b2e84717b315c35a92e9192639ac8272b3f3c
Sardorbekmaxmud <sardorbekmaxmudov33@gmail.com>
2025-02-19 23:19:00 +0500

    edit models and change some mistakes

--- to_do/admin.py
+++ to_do/admin.py
@@ -5,8 +5,8 @@ from .models import ToDo, ToDoRepeat, ToDoHistory
 # Register your models here.
 @admin.register(ToDo)
 class ToDoAdmin(admin.ModelAdmin):
-    list_display = ('author', 'body', 'done', 'created_at', 'updated_at')
-    list_filter = ('created_at', 'author__username', 'done')
+    list_display = ('author', 'body', 'created_at', 'updated_at')
+    list_filter = ('created_at', 'author__username')
     search_fields = ('body', 'author__username')
     ordering = ('created_at',)
     readonly_fields = ['created_at', 'updated_at']
@@ -15,16 +15,16 @@ class ToDoAdmin(admin.ModelAdmin):
 @admin.register(ToDoRepeat)
 class ToDoRepeatAdmin(admin.ModelAdmin):
     list_display = ('todo__body', 'repeat_day')
-    list_filter = ('repeat_day',)
+    list_filter = ('repeat_day', 'todo__body')
     search_fields = ('todo__body',)
 
 
 @admin.register(ToDoHistory)
 class ToDoHistoryAdmin(admin.ModelAdmin):
-    list_display = ('todo__body', 'complete_date', 'updated_at')
-    list_filter = ('complete_date',)
-    ordering = ('complete_date', 'updated_at')
-    readonly_fields = ['complete_date', 'updated_at']
+    list_display = ('todo__body', 'status', 'date', 'updated_at')
+    list_filter = ('date', 'status', 'todo__body')
+    ordering = ('status', 'date')
+    readonly_fields = ['date', 'updated_at']
 
     class Meta:
         db_name = 'todo_history'
--- to_do/views/todo_detail.py
+++ to_do/views/todo_detail.py
@@ -1,25 +1,31 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views import View
 from django.shortcuts import render
-from django.db.models import Count
+from django.db.models import Count, Q
+from django.db.models.functions import Coalesce
 from django.db.models.functions import TruncYear, TruncMonth
 from django.utils import timezone
 import json
-import random
 
 from to_do.models import ToDo
+from to_do.services import get_todo_by_id
 
 
 class ToDoDetailView(LoginRequiredMixin, View):
     def get(self, request, todo_id):
-        todo = ToDo.objects.filter(pk=todo_id, author=request.user).first()
+        todo = get_todo_by_id(todo_id)
+
+        todo['created_at'] = timezone.localtime(timezone.make_aware(todo['created_at']))
+        todo['updated_at'] = timezone.localtime(timezone.make_aware(todo['updated_at']))
 
         yearly_statistics = (
             ToDo.objects.prefetch_related('todo_histories')
-            .values(year=TruncYear("todo_histories__complete_date"))
-            .annotate(completed=Count('*'))
             .filter(pk=todo_id, author=request.user)
-            .order_by('year')
+            .values(year=TruncYear("todo_histories__date"))
+            .annotate(
+                completed=Coalesce(Count('todo_histories__status', filter=Q(todo_histories__status=True)), 0),
+                pending=Coalesce(Count('todo_histories__status', filter=Q(todo_histories__status=False)), 0)
+            ).order_by('year')
         )
 
         year = timezone.now().year
@@ -28,19 +34,20 @@ class ToDoDetailView(LoginRequiredMixin, View):
             ToDo.objects.prefetch_related('todo_histories')
             .filter(pk=todo_id,
                     author=request.user,
-                    todo_histories__complete_date__year=str(year))
-            .values(month=TruncMonth("todo_histories__complete_date"))
-            .annotate(completed=Count('*'))
-            .order_by('month')
+                    todo_histories__date__year=str(year))
+            .values(month=TruncMonth("todo_histories__date"))
+            .annotate(
+                completed=Coalesce(Count('todo_histories__status', filter=Q(todo_histories__status=True)), 0),
+                pending=Coalesce(Count('todo_histories__status', filter=Q(todo_histories__status=False)), 0)
+            ).order_by('month')
         )
-        # print(monthly_statistics.query)
 
         # Yillik statistik ma'lumotlar
         try:
             yearly_data = []
             for data in yearly_statistics:
                 data['year'] = data['year'].strftime('%Y')
-                yearly_data.append({'year': data['year'], 'completed': data['completed'], 'pending': random.randint(1, 10)})
+                yearly_data.append({'year': data['year'], 'completed': data['completed'], 'pending': data['pending']})
         except AttributeError:
             yearly_data = []
 
@@ -52,7 +59,7 @@ class ToDoDetailView(LoginRequiredMixin, View):
             monthly_data = []
             for data in monthly_statistics:
                 data['month'] = data['month'].strftime('%m')
-                monthly_data.append({'month': uz_months[data['month']], 'completed': data['completed'], 'pending': random.randint(1, 10)})
+                monthly_data.append({'month': uz_months[data['month']], 'completed': data['completed'], 'pending': data['pending']})
         except AttributeError:
             monthly_data = []
 
