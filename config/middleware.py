ed3ac94bb7b0b361b32e1f78f57963ecff32e6ae
Sardorbekmaxmud <sardorbekmaxmudov33@gmail.com>
2025-03-05 11:47:11 +0500

    edit some mistakes

--- config/middleware.py
+++ config/middleware.py
@@ -1,6 +1,5 @@
 from django.db.models import Q
 from django.utils.timezone import now
-from django.core.cache import cache
 
 from to_do.models import ToDo, ResetTodos
 
@@ -16,13 +15,16 @@ class ResetTodosMiddleware:
         except ResetTodos.DoesNotExist:
             last_reset = None
 
-        if last_reset.date is None or last_reset.date < today:
+        if last_reset is None:
+            response = self.get_response(request)
+            return response
+
+        elif last_reset.date is None or last_reset.date < today:
             today_week_num = now().weekday()
 
             ToDo.objects.prefetch_related('todo_repeats').filter(
                 Q(todo_repeats__repeat_day=today_week_num) &
                 Q(status=True) | Q(status=False)).update(status=None)
-            # cache.set("last_todos_reset", today, timeout=86400)  # 24 soat cache
             ResetTodos.objects.create(date=today)
 
         response = self.get_response(request)
